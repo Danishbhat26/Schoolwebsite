fcebc9ed0ba408e31e56c9dfe3dc15a9075fb389
Danishbhat26 <erdanishbhat00@gmail.com>
2026-07-25 10:33:07 +0530

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,5 +1,26 @@
-document.getElementById("logout").addEventListener("click",()=>{
+import { auth } from "./firebase-config.js";
 
-    alert("Logout feature will be added next.");
+import {
+    onAuthStateChanged,
+    signOut
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+// Protect Dashboard
+onAuthStateChanged(auth, (user) => {
+
+    if (!user) {
+
+        window.location.href = "admin.html";
+
+    }
+
+});
+
+// Logout
+document.getElementById("logout").addEventListener("click", async () => {
+
+    await signOut(auth);
+
+    window.location.href = "admin.html";
 
 });
